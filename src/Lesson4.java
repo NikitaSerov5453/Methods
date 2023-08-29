c23505e688941f492639f26200a8a18bb67f9f28
dante5453 <nikita.serov5453@gmail.com>
2023-08-29 16:32:29 +0300

    compression

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -26,11 +28,18 @@ public class Lesson4 {
         generateTriangle(triangleArray);
         printTriangle(triangleArray);
         generateTriangleMatrix(triangleMatrix);
-        printTriangle(triangleMatrix);
-        stepDefinition();
+        fieldTriangleMatrix(triangleMatrix);
+//        go();
+//        printTriangle(triangleMatrix);
+        generateTriangleMatrix(triangleSupportMatrix);
+        printTriangle(triangleArrayHelp);
+        compression(triangleArrayHelp);
+        printTriangle(triangleArrayHelp);
 
     }
-    public static final int[] row = new int[]{0,1};
+
+    public static final int[] row = new int[]{0, 1};
+    public static final int[] cel = new int[]{1, 1};
     public static int x = 0;
     public static int y = 0;
     public static int minLengthTriangle = 2;
@@ -38,15 +47,19 @@ public class Lesson4 {
     public static int minNumberInTriangle = 0;
     public static int maxNumberInTriangle = 100;
 
-    public static int lengthArray = random(minLengthTriangle, maxLengthTriangle);
+    public static int lengthArray = 5;
 
     public static int[][] triangleArray = new int[lengthArray][];
+    public static int[][] triangleArrayHelp = new int[][]{{60},{80, 28},{16, 94, 42},{15, 45, 2, 33},{98, 7, 93, 3, 79}};
     public static int[][] triangleMatrix = new int[lengthArray][];
+    public static int[][] triangleSupportMatrix = new int[lengthArray][];
     public static int[] wayArray = new int[lengthArray];
+    public static int[] holdWayArray = new int[lengthArray];
 
-    public static boolean isValid(int x, int y) {
-        return x >= 0 && y >= 0 && x < lengthArray && y < lengthArray;
+    public static boolean isValid(int x, int y, int[] array) {
+        return x > 0 && y > 0 && x < lengthArray && y < array.length;
     }
+
     public static void generateTriangle(int[][] triangleArray) {
         for (int i = 0; i < triangleArray.length; i++) {
             triangleArray[i] = generateArray(i + 1);
@@ -69,9 +82,9 @@ public class Lesson4 {
         return array;
     }
 
-    public static void generateTriangleMatrix(int[][] triangleArray) {
-        for (int i = 0; i < triangleArray.length; i++) {
-            triangleArray[i] = generateMatrixArray(i + 1);
+    public static void generateTriangleMatrix(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = generateMatrixArray(i + 1);
         }
     }
 
@@ -86,36 +99,165 @@ public class Lesson4 {
         return ThreadLocalRandom.current().nextInt(minValue, maxValue);
     }
 
-    public static void counterMaxNumber(int[][] array) {
+    public static void go() {
+        int holdCounter = 0;
         int counter = 0;
-        int finalSum = 0;
+        for (int i = 0; i < triangleMatrix.length; i++) {
+            for (int j = 0; j < triangleMatrix[i].length; j++) {
+                if (triangleMatrix[i][j] == minValueInArray(triangleMatrix[i]) && triangleMatrix[i][j] != 0) {
+                    counter += triangleArray[i][j];
+                    holdWayArray[i] = triangleArray[i][j];
+
+                }
+            }
+        }
+
+        if (counter > holdCounter) {
+            holdCounter = counter;
+            wayArray = holdWayArray;
+        }
+        System.out.println(holdCounter);
+        System.out.println(Arrays.toString(wayArray));
+        counter = 0;
+        minusArray(triangleMatrix);
+        if (triangleMatrix[0][0] == 0) {
+            return;
+        }
+
+        go();
+    }
+
+    public static int minValueInArray(int[] array) {
+        int minValue = Integer.MAX_VALUE;
+        for (int j : array) {
+            if (j < minValue && j != 0) {
+                minValue = j;
+            }
+        }
+        return minValue;
+    }
+
+//    public static void counterMaxNumber(int[][] array) {
+//        int counter = 0;
+//        int finalSum = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < i + 1; j++) {
+//
+//            }
+//        }
+//    }
+
+//    public static void stepDefinition(int[][] triangleMatrix) {
+//
+//        int holdX = 0;
+//        int holdY = 0;
+//
+//        for (int i = 0; i < 2; i++) {
+//            int newX = x++;
+//            int newY = y + row[i];
+//            if (isValid(x, y)) {
+//                if (triangleMatrix[newX][newY] < triangleMatrix[holdX][holdY] && triangleMatrix[newX][newY] != 0) {
+//                    holdY = newY;
+//                    holdX = newX;
+//                }
+//            }
+//        }
+//        x = holdX;
+//        y = holdY;
+//        triangleMatrix[x][y] = triangleMatrix[x][y] - 1;
+//        if (triangleMatrix[0][0] == 0) {
+//            return;
+//        }
+//        stepDefinition(triangleMatrix);
+//    }
+
+
+    public static int pow(int value, int powValue) {
+        int result = 1;
+        for (int i = 1; i <= powValue; i++) {
+            result = result * value;
+        }
+        return result;
+    }
+
+    public static void fieldTriangleMatrix(int[][] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < i + 1; j++) {
-                counter = counter + array[i][j];
-                if (finalSum < counter) {
-                    finalSum = counter;
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = pow(2, array.length - (i + 1));
+                if (j != 0) {
+                    array[i][j] = array[i][j - 1] * 2;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (j != 0 && j != array[i].length - 1) {
+                    array[i][j]++;
                 }
             }
         }
     }
 
-    public static void stepDefinition() {
-        int holdX = 0;
-        int holdY = 0;
-        for (int i = 0; i < 2; i++) {
-            int newX = x + row[i];
-            int newY = y++;
-            if (isValid(x, y)) {
-                holdY = newY;
-                holdX = newX;
+    public static void fieldTriangleSupportMatrix(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j]++;
+                if (i < array.length - 1) {
+                    array[i + 1][j]++;
+                    array[i + 1][j + 1]++;
+                }
             }
         }
-        System.out.println(x);
-        System.out.println(y);
-        x = holdX;
-        y = holdY;
-        System.out.println(x);
-        System.out.println(y);
     }
 
+    public static void minusArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > 0) {
+                    array[i][j]--;
+                }
+            }
+        }
+    }
+
+
+    public static ArrayList<Integer> array1 = new ArrayList<>();
+
+    public static void compression(int[][] array) {
+        int hold = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (j == 0) {
+                    a += array[i][j];
+                    array[i][j] = a;
+                }
+                if (j == array[i].length - 1) {
+                    b += array[i][j];
+                    array[i][j] = b;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (j > 0 && j < array[i].length - 1) {
+                    hold = Math.max(array[i - 1][j], array[i - 1][j - 1]);
+                    array[i][j] = hold + array[i][j];
+
+                }
+
+            }
+        }
+    }
+//    public static void x(int[][] array) {
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                array[i][j] = pow(2, array.length - (i + 1));
+//                if (j != 0 && j != array[i].length - 1) {
+//                    array[i][j]++;
+//                }
+//            }
+//        }
+//    }
 }
